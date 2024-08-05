336d2452eddb58687e1bc7119b181f47bb3e0b3f
yondikavl <yondikaviolanda@gmail.com>
2024-08-05 22:04:27 +0700

    fix: car details (card + profile)

--- lib/presentation/view/car_details_screen.dart
+++ lib/presentation/view/car_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rentcar_app/data/models/Car.dart';
+import 'package:rentcar_app/presentation/widgets/car_card.dart';
 
 class CarDetailsScreen extends StatelessWidget {
   const CarDetailsScreen({super.key});
@@ -11,9 +13,46 @@ class CarDetailsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(Icons.info_outline),
-            Text(' Information'),
+            Text(' Informasi'),
           ],
         ),
+      ),
+      body: Column(
+        children: [
+          CarCard(
+            car: Car(
+                model: 'Fortuner GR',
+                distance: 800,
+                fuelCapacity: 60,
+                pricePerHour: 50),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage(
+                    'assets/user.png',
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Yondi Kavio',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '\$4.567',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
